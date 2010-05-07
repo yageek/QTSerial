beaf0e8a6318013f7a8d532790ada125c4c9edfd
HEINRICH Yannick <yaya@YannickHeinrich.(none)>
2010-05-07 11:10:18 +0200

    Add translations

--- main.cpp
+++ main.cpp
@@ -1,9 +1,17 @@
 #include <QtGui/QApplication>
 #include "serialtool.h"
 #include<iostream>
+#include<QTranslator>
 int main(int argc, char *argv[])
 {
    QApplication a(argc, argv);
+
+   QString locale = QLocale::system().name().section('_', 0, 0);
+
+     QTranslator translator;
+     translator.load(QString("qtserial_") + locale);
+     a.installTranslator(&translator);
+
    SerialTool serial;
    serial.setVisible(true);
 
--- qtserial_en.ts
+++ qtserial_en.ts
@@ -6,183 +6,183 @@
     <message>
         <location filename="serialtool.ui" line="17"/>
         <source>Serial Tool</source>
-        <translation type="unfinished"></translation>
+        <translation>Serial Tool</translation>
     </message>
     <message>
         <location filename="serialtool.ui" line="33"/>
         <source>/dev/ttyUSB0</source>
-        <translation type="unfinished"></translation>
+        <translation>/dev/ttyUSB0</translation>
     </message>
     <message>
         <location filename="serialtool.ui" line="41"/>
         <source>Refresh Port</source>
-        <translation type="unfinished"></translation>
+        <translation>Refresh Port</translation>
     </message>
     <message>
         <location filename="serialtool.ui" line="48"/>
         <source>Open</source>
-        <translation type="unfinished"></translation>
+        <translation>Open</translation>
     </message>
     <message>
         <location filename="serialtool.ui" line="55"/>
         <source>Close</source>
-        <translation type="unfinished"></translation>
+        <translation>Close</translation>
     </message>
     <message>
         <location filename="serialtool.ui" line="62"/>
         <source>...</source>
-        <translation type="unfinished"></translation>
+        <translation>...</translation>
     </message>
     <message>
         <location filename="serialtool.ui" line="73"/>
         <source>ASCII</source>
-        <translation type="unfinished"></translation>
+        <translation>ASCII</translation>
     </message>
     <message>
         <location filename="serialtool.ui" line="83"/>
         <source>Terminal mode</source>
-        <translation type="unfinished"></translation>
+        <translation>Terminal.modes</translation>
     </message>
     <message>
         <location filename="serialtool.ui" line="90"/>
         <source>Commandes Hayes</source>
-        <translation type="unfinished"></translation>
+        <translation>Hayes command</translation>
     </message>
     <message>
         <location filename="serialtool.ui" line="101"/>
         <source>Baudrate</source>
-        <translation type="unfinished"></translation>
+        <translation>Baudrate</translation>
     </message>
     <message>
         <location filename="serialtool.ui" line="112"/>
         <source>9600</source>
-        <translation type="unfinished"></translation>
+        <translation>9600</translation>
     </message>
     <message>
         <location filename="serialtool.ui" line="117"/>
         <source>300</source>
-        <translation type="unfinished"></translation>
+        <translation>300</translation>
     </message>
     <message>
         <location filename="serialtool.ui" line="122"/>
         <source>600</source>
-        <translation type="unfinished"></translation>
+        <translation>600</translation>
     </message>
     <message>
         <location filename="serialtool.ui" line="127"/>
         <source>1200</source>
-        <translation type="unfinished"></translation>
+        <translation>1200</translation>
     </message>
     <message>
         <location filename="serialtool.ui" line="132"/>
         <source>2400</source>
-        <translation type="unfinished"></translation>
+        <translation>2400</translation>
     </message>
     <message>
         <location filename="serialtool.ui" line="137"/>
         <source>4800</source>
-        <translation type="unfinished"></translation>
+        <translation>4800</translation>
     </message>
     <message>
         <location filename="serialtool.ui" line="142"/>
         <source>19200</source>
-        <translation type="unfinished"></translation>
+        <translation>19200</translation>
     </message>
     <message>
         <location filename="serialtool.ui" line="147"/>
         <source>38400</source>
-        <translation type="unfinished"></translation>
+        <translation>38400</translation>
     </message>
     <message>
         <location filename="serialtool.ui" line="152"/>
         <source>57600</source>
-        <translation type="unfinished"></translation>
+        <translation>57600</translation>
     </message>
     <message>
         <location filename="serialtool.ui" line="157"/>
         <source>115200</source>
-        <translation type="unfinished"></translation>
+        <translation>115200</translation>
     </message>
     <message>
         <location filename="serialtool.ui" line="165"/>
         <source>Parity</source>
-        <translation type="unfinished"></translation>
+        <translation>Parity</translation>
     </message>
     <message>
         <location filename="serialtool.ui" line="173"/>
         <location filename="serialtool.ui" line="251"/>
         <source>None</source>
-        <translation type="unfinished"></translation>
+        <translation>None</translation>
     </message>
     <message>
         <location filename="serialtool.ui" line="178"/>
         <source>Even</source>
-        <translation type="unfinished"></translation>
+        <translation>Even</translation>
     </message>
     <message>
         <location filename="serialtool.ui" line="183"/>
         <source>Odd</source>
-        <translation type="unfinished"></translation>
+        <translation>Odd</translation>
     </message>
     <message>
         <location filename="serialtool.ui" line="191"/>
         <source>Bits</source>
-        <translation type="unfinished"></translation>
+        <translation>Bits</translation>
     </message>
     <message>
         <location filename="serialtool.ui" line="199"/>
         <source>8</source>
-        <translation type="unfinished"></translation>
+        <translation>8</translation>
     </message>
     <message>
         <location filename="serialtool.ui" line="204"/>
         <source>5</source>
-        <translation type="unfinished"></translation>
+        <translation>5</translation>
     </message>
     <message>
         <location filename="serialtool.ui" line="209"/>
         <source>6</source>
-        <translation type="unfinished"></translation>
+        <translation>6</translation>
     </message>
     <message>
         <location filename="serialtool.ui" line="214"/>
         <source>7</source>
-        <translation type="unfinished"></translation>
+        <translation>7</translation>
     </message>
     <message>
         <location filename="serialtool.ui" line="222"/>
         <source>Stopbits</source>
-        <translation type="unfinished"></translation>
+        <translation>Number of stop bits</translation>
     </message>
     <message>
         <location filename="serialtool.ui" line="230"/>
         <source>1</source>
-        <translation type="unfinished"></translation>
+        <translation>1</translation>
     </message>
     <message>
         <location filename="serialtool.ui" line="235"/>
         <source>2</source>
-        <translation type="unfinished"></translation>
+        <translation>2</translation>
     </message>
     <message>
         <location filename="serialtool.ui" line="243"/>
         <source>Flow control</source>
-        <translation type="unfinished"></translation>
+        <translation>Flow control</translation>
     </message>
     <message>
         <location filename="serialtool.ui" line="256"/>
         <source>Hardware</source>
-        <translation type="unfinished"></translation>
+        <translation>Hardware</translation>
     </message>
     <message>
         <location filename="serialtool.ui" line="261"/>
         <source>Software</source>
-        <translation type="unfinished"></translation>
+        <translation>Software</translation>
     </message>
     <message>
         <location filename="serialtool.ui" line="266"/>
         <source>Space</source>
-        <translation type="unfinished"></translation>
+        <translation>Space</translation>
     </message>
 </context>
 </TS>
