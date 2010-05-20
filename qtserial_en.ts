338d834c74feac8c18de4fd7c8879574374c1cea
YaGeek <yageek@free.fr>
2010-05-20 09:30:51 +0200

    Change error in language file

--- qtserial_en.ts
+++ qtserial_en.ts
@@ -1,206 +1,163 @@
-<?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS>
-<TS version="2.0" language="en_US">
+<!DOCTYPE TS><TS>
 <context>
     <name>SerialTool</name>
     <message>
-        <location filename="serialtool.ui" line="17"/>
         <source>Serial Tool</source>
         <translation>Serial Tool</translation>
     </message>
     <message>
-        <location filename="serialtool.ui" line="33"/>
         <source>/dev/ttyUSB0</source>
         <translation>/dev/ttyUSB0</translation>
     </message>
     <message>
-        <location filename="serialtool.ui" line="41"/>
         <source>Refresh Port</source>
         <translation>Refresh Port</translation>
     </message>
     <message>
-        <location filename="serialtool.ui" line="48"/>
         <source>Open</source>
         <translation>Open</translation>
     </message>
     <message>
-        <location filename="serialtool.ui" line="55"/>
         <source>Close</source>
         <translation>Close</translation>
     </message>
     <message>
-        <location filename="serialtool.ui" line="62"/>
         <source>...</source>
         <translation>...</translation>
     </message>
     <message>
-        <location filename="serialtool.ui" line="73"/>
         <source>Send text in the line below</source>
         <translation>Send text in the line below</translation>
     </message>
     <message>
-        <location filename="serialtool.ui" line="76"/>
         <source>ASCII</source>
         <translation>ASCII</translation>
     </message>
     <message>
-        <location filename="serialtool.ui" line="86"/>
         <source>Send text in the line below + carriage return (\n)</source>
         <translation>Envoie le texte de la ligne en bas</translation>
     </message>
     <message>
-        <location filename="serialtool.ui" line="89"/>
         <source>Terminal mode</source>
         <translation>Terminal.modes</translation>
     </message>
     <message>
-        <location filename="serialtool.ui" line="96"/>
         <source>Send text in the line below + backspace (\r)</source>
         <translation>Send text in the line below + backspace (\r)</translation>
     </message>
     <message>
-        <location filename="serialtool.ui" line="99"/>
         <source>Commandes Hayes</source>
         <translation>Hayes command</translation>
     </message>
     <message>
-        <location filename="serialtool.ui" line="110"/>
         <source>Baudrate</source>
         <translation>Baudrate</translation>
     </message>
     <message>
-        <location filename="serialtool.ui" line="121"/>
         <source>9600</source>
         <translation>9600</translation>
     </message>
     <message>
-        <location filename="serialtool.ui" line="126"/>
         <source>300</source>
         <translation>300</translation>
     </message>
     <message>
-        <location filename="serialtool.ui" line="131"/>
         <source>600</source>
         <translation>600</translation>
     </message>
     <message>
-        <location filename="serialtool.ui" line="136"/>
         <source>1200</source>
         <translation>1200</translation>
     </message>
     <message>
-        <location filename="serialtool.ui" line="141"/>
         <source>2400</source>
         <translation>2400</translation>
     </message>
     <message>
-        <location filename="serialtool.ui" line="146"/>
         <source>4800</source>
         <translation>4800</translation>
     </message>
     <message>
-        <location filename="serialtool.ui" line="151"/>
         <source>19200</source>
         <translation>19200</translation>
     </message>
     <message>
-        <location filename="serialtool.ui" line="156"/>
         <source>38400</source>
         <translation>38400</translation>
     </message>
     <message>
-        <location filename="serialtool.ui" line="161"/>
         <source>57600</source>
         <translation>57600</translation>
     </message>
     <message>
-        <location filename="serialtool.ui" line="166"/>
         <source>115200</source>
         <translation>115200</translation>
     </message>
     <message>
-        <location filename="serialtool.ui" line="174"/>
         <source>Parity</source>
         <translation>Parity</translation>
     </message>
     <message>
-        <location filename="serialtool.ui" line="182"/>
-        <location filename="serialtool.ui" line="260"/>
         <source>None</source>
         <translation>None</translation>
     </message>
     <message>
-        <location filename="serialtool.ui" line="187"/>
         <source>Even</source>
         <translation>Even</translation>
     </message>
     <message>
-        <location filename="serialtool.ui" line="192"/>
         <source>Odd</source>
         <translation>Odd</translation>
     </message>
     <message>
-        <location filename="serialtool.ui" line="200"/>
         <source>Bits</source>
         <translation>Bits</translation>
     </message>
     <message>
-        <location filename="serialtool.ui" line="208"/>
         <source>8</source>
         <translation>8</translation>
     </message>
     <message>
-        <location filename="serialtool.ui" line="213"/>
         <source>5</source>
         <translation>5</translation>
     </message>
     <message>
-        <location filename="serialtool.ui" line="218"/>
         <source>6</source>
         <translation>6</translation>
     </message>
     <message>
-        <location filename="serialtool.ui" line="223"/>
         <source>7</source>
         <translation>7</translation>
     </message>
     <message>
-        <location filename="serialtool.ui" line="231"/>
         <source>Stopbits</source>
         <translation>Number of stop bits</translation>
     </message>
     <message>
-        <location filename="serialtool.ui" line="239"/>
         <source>1</source>
         <translation>1</translation>
     </message>
     <message>
-        <location filename="serialtool.ui" line="244"/>
         <source>2</source>
         <translation>2</translation>
     </message>
     <message>
-        <location filename="serialtool.ui" line="252"/>
         <source>Flow control</source>
         <translation>Flow control</translation>
     </message>
     <message>
-        <location filename="serialtool.ui" line="265"/>
         <source>Hardware</source>
         <translation>Hardware</translation>
     </message>
     <message>
-        <location filename="serialtool.ui" line="270"/>
         <source>Software</source>
         <translation>Software</translation>
     </message>
     <message>
-        <location filename="serialtool.ui" line="275"/>
         <source>Space</source>
         <translation>Space</translation>
     </message>
     <message>
-        <location filename="serialtool.cpp" line="38"/>
         <source>Enreg last parameters</source>
         <translation>Enreg last parameters</translation>
     </message>
